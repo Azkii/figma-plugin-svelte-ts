3e5e1f96282575c55c361157dca8b39d221af249
Dawid <Dawid_pacanowski@wp.pl>
2023-10-21 21:54:17 +0200

    feat: bundle visualizer added

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,26 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { viteSingleFile } from "vite-plugin-singlefile";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [svelte(), viteSingleFile()],
-  root: "./ui-app",
-  build: {
-    outDir: "../dist",
-  },
-});
+export default () => {
+  const isAnalyzeOn = process.env.ANALYZE === "true";
+
+  if (isAnalyzeOn) {
+    return defineConfig({
+      plugins: [svelte(), , tsconfigPaths()],
+      root: "./ui-app",
+      build: {
+        outDir: "../.analyze",
+      },
+    });
+  }
+
+  return defineConfig({
+    plugins: [svelte(), viteSingleFile(), tsconfigPaths()],
+    root: "./ui-app",
+    build: {
+      outDir: "../dist",
+    },
+  });
+};
